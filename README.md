ed5ca3545afcfac7e64fd6613d1190b012055ecf
Sebastian Salvo <sebysalvo@gmail.com>
2016-12-15 00:22:39 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Website
+Final Project for Webware by Sebastian Salvo
+. Massachusetts tourism site
+
+ I designed this website to be used as a tourism site for people wanting to visit Massachusetts.
+
+Massachusetts picture used on the front page as a reference:
+http://www.mass.gov/anf/images/itd/massgis/datalayers/cong113.png
---
+++ server.js
@@ -0,0 +1,48 @@
+var http = require('http')
+ , fs 	 = require('fs')
+ , url   = require('url')
+ , path  = require('pathname')
+ , qs    = require('querystring')
+ , port  = 8080;
+
+var server = http.createServer (function (req, res) {
+	var uri = url.parse(req.url);
+	var query = url.parse(req.url).query;
+	console.log(uri.pathname);
+
+	switch( uri.pathname ) {
+		case '/':
+			sendFile(res, 'index.html', 'text/html')
+			break
+		case '/index.html':
+			sendFiles(res, 'index.html', 'text/html')
+			break
+		case '/style.css':
+			sendFile(res, 'style.css', 'text/css')
+			break
+		case '/README.md':
+			sendFile(res, 'README.md', 'text/markdown')
+			break
+		case '/package.json':
+			sendFile(res, 'package.json', 'text/json')
+			break
+		default:
+			res.end('404 not found')
+	}
+
+})
+
+
+server.listen(process.env.PORT || port)
+console.log('listening on ' + port)
+
+//subroutines
+
+function sendFile(res, filename, contentType) {
+	contentType = contentType || 'text/html'
+
+	fs.readFile(filename, function(error, content) {
+		res.writeHead(200, {'Content-type': contentType})
+		res.end(content, 'utf-8')
+	})
+}
