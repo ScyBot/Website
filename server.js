6ece153d99e711d58ad60c5ce11d94fcaadad0a6
Sebastian Salvo <sebysalvo@gmail.com>
2016-12-15 04:15:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var http = require('http')
  , qs    = require('querystring')
  , port  = 8080;
 
+var places = ['Barnstable', 'Berkshire', 'Bristol', 'Dukes', 'Essex', 'Franklin', 'Hampden', 'Hampshire', 'Middlesex', 'Nantucket', 'Norfolk', 'Plymouth', 'Suffolk', 'Worcester']
+
 var server = http.createServer (function (req, res) {
 	var uri = url.parse(req.url);
 	console.log(uri.pathname);
@@ -11,6 +13,9 @@ var server = http.createServer (function (req, res) {
 	switch( uri.pathname ) {
 		case '/':
 			sendFile(res, 'index.html', 'text/html')
+			break
+		case '#':
+			handleSearch(res,uri)
 			break
 		case '/index.html':
 			sendFiles(res, 'index.html', 'text/html')
@@ -45,7 +50,7 @@ function sendFile(res, filename, contentType) {
 	})
 }
 
-/* function handleSearch(res, uri) {
+function handleSearch(res, uri) {
 	var contentType = 'text/html'
   	res.writeHead(200, {'Content-type': contentType})
 	var contentType = 'text/html'
@@ -54,14 +59,43 @@ function sendFile(res, filename, contentType) {
   	html = html + '<html>'
 
   	html = html + '<head>'
-  	// You could add a CSS and/or js call here...
   	html = html + '<link rel="stylesheet" type="text/css" href="style.css"/>'
   	html = html + '</head>'
 
   	html = html + '<body>'
-  	html = html + '<h1> Search </h1>'
+  	html = html + '<h1> Specific County Search </h1>'
   	html = html + '<form action="search" method="GET">'
   	html = html + '<input type="text" name="search" />'
   	html = html + '<button type="submit">Search</button>'
   	html = html + '</form>'
-	*/
+    if(uri.query) {
+    console.log( uri.query ) //just to check
+    var data = qs.parse(uri.query)
+    if(data.search){
+      if(places.indexOf(data.search) > -1){
+        html = html + '<ul>';
+        for(var i = 0; i < places.length; i++){
+          if(places[i].includes(data.search)){
+            html+= "<li>" + places[i] + "</li>"; 
+          }
+        }
+        html = html + '</ul>'
+      }
+      else{
+        html+="<p>No results found for that place</p>"
+      }
+    }
+    else{
+      html+="<p>Invalid query</p>"
+    }
+  } else {
+    //if they dont provide anything at all
+    html+="<p>No query provided</p>"
+  }
+
+    html = html + '</body>'
+  html = html + '</html>'
+  
+  res.writeHead(200, {'Content-type': contentType})
+  res.end(html, 'utf-8')
+}
